eae2bdbe1668ca9733169bc8d82cc7b94234262b
Pabitra Mondal <83583681+P-kaizoku@users.noreply.github.com>
2024-12-28 17:47:17 +0530

    Code by me

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+function WeatherApp() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState({});
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+//   console.log(process.env);
+
+  function handleInputChange(e) {
+    setInput(e.target.value);
+  }
+
+  async function searchCity() {
+    if (input.trim() === "") {
+      alert("Please enter the city!");
+      return;
+    }
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${input}&aqi=yes`
+      );
+      if (!response.ok) {
+        throw new Error(
+          `HTTP Error: ${response.status} - ${response.statusText}`
+        );
+      }
+
+      const data = await response.json();
+      setResult(data);
+      setInput("");
+    } catch (error) {
+      setResult({ Error: error.message });
+    }
+  }
+
+  return (
+    <>
+      <h2>Weather-Forecaster</h2>
+
+      <div className="main">
+        <input
+          type="text"
+          placeholder="Enter the city..."
+          id="city"
+          value={input}
+          onChange={handleInputChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchCity();
+            }
+          }}
+        />
+
+        <button type="submit" id="search-btn" onClick={searchCity}>
+          Search
+        </button>
+      </div>
+
+      <div className="display">
+        {result.current && (
+          <div>
+            <h3>
+              {result.location.name}, {result.location.country}
+            </h3>
+            <p>Temperature: {result.current.temp_c}°C</p>
+            <p>Condition: {result.current.condition.text}</p>
+            <img src={result.current.condition.icon} alt="weather icon" />
+            <p>Humidity: {result.current.humidity}%</p>
+            <p>Wind: {result.current.wind_kph} kph</p>
+          </div>
+        )}
+        {result.Error && (
+          <div>
+            <p>{result.Error}</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default WeatherApp;
