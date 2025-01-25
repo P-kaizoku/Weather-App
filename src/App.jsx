6bb58d0554358bd1f5bb55c8ee3335a8f7f4deca
Pabitra Mondal <83583681+P-kaizoku@users.noreply.github.com>
2025-01-25 14:49:52 +0530

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import WeatherApp from './WeatherApp'
+import WeatherApp from "./WeatherApp";
 
-
-function App(){
-    return (
-      <>
-        <WeatherApp />
-      </>
-    )
+function App() {
+  return (
+    <div className="flex  justify-center items-center w-screen h-screen bg-red-300">
+      <WeatherApp />
+    </div>
+  );
 }
 
 export default App;
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -35,11 +35,12 @@ function WeatherApp() {
   }
 
   return (
-    <>
-      <h2>Weather-Forecaster</h2>
+    <div className="bg-neutral-600 min-h-3/4 h-max w-3/5 text-white p-6 flex flex-col items-center  rounded-xl drop-shadow-[0_35px_35px_rgba(255,255,255,0.50)] ">
+      <h2 className="w-3/4 p-2 text-center m-4 font-bold text-2xl ">Weather-Forecaster</h2>
 
-      <div className="main">
+      <div className="main bg-neutral-500 px-4 py-6 rounded-2xl flex gap-4">
         <input
+          className=" outline-none text-lg"
           type="text"
           placeholder="Enter the city..."
           id="city"
@@ -57,17 +58,17 @@ function WeatherApp() {
         </button>
       </div>
 
-      <div className="display">
+      <div className="display bg-neutral-500 m-6 p-6 w-3/4 rounded-2xl">
         {result.current && (
           <div>
-            <h3>
+            <h3 className="font-bold text-xl">
               {result.location.name}, {result.location.country}
             </h3>
-            <p>Temperature: {result.current.temp_c}°C</p>
-            <p>Condition: {result.current.condition.text}</p>
-            <img src={result.current.condition.icon} alt="weather icon" />
-            <p>Humidity: {result.current.humidity}%</p>
-            <p>Wind: {result.current.wind_kph} kph</p>
+            <p className="mt-2 font-mono">Temperature: {result.current.temp_c}°C</p>
+            <p className="font-mono">Condition: {result.current.condition.text}</p>
+            <img className="" src={result.current.condition.icon} alt="weather icon" />
+            <p className="font-mono">Humidity: {result.current.humidity}%</p>
+            <p className="font-mono">Wind: {result.current.wind_kph} kph</p>
           </div>
         )}
         {result.Error && (
@@ -76,7 +77,7 @@ function WeatherApp() {
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 }
 
